17c57fe9439f883b7fedecc10b7a8b28a8f3ea26
charlotte <charlottemaemorrison@gmail.com>
2019-08-18 13:13:25 -0400

    Initial commit

---
+++ td3-algorithm/Actor.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Actor(nn.module):
+    def __init__(self, state_dim, action_dim, max_action):
+        """Create the Actor neural network layers."""
+        super(Actor, self).__init__()
+
+        self.layer1 = nn.Linear(state_dim, 400)
+        self.layer2 = nn.Linear(400, 300)
+        self.layer3 = nn.Linear(300, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, x):
+        """Forward pass in Actor neural network."""
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x = self.max_action * torch.tanh(self.layer3(x))
+        return x
